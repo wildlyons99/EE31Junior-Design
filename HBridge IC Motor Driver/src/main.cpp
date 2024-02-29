2a16fe42c8180506f3d244bc52fbb841279f7ca6
Tom Lyons <thomas.lyons@tufts.edu>
2024-02-29 01:38:15 -0500

    HBridge IC testing / implementation code

---
+++ HBridge IC Motor Driver/src/main.cpp
@@ -0,0 +1,104 @@
+// main.cpp
+// Tom Lyons
+// 
+// EE31 Junior Design - phase 2B
+// Tufts University 
+
+#include <Arduino.h>
+
+// PWM pin options: 3, 5, 6, 9, 10 and 11
+ 
+// Motor A connections
+const int enableA = 9; // pwm pin
+const int motorA1 = 5;
+const int motorA2 = 6;
+ 
+// Motor B connections
+const int enableB = 10; // pwm pin
+const int motorB1 = 7;
+const int motorB2 = 8;
+ 
+// Set the speed (0 = off and 255 = max speed)
+// will likely need to make seperate motor speed for A and B to match so: 
+const int motorSpeedA = 128; // 50% duty cycle 
+const int motorSpeedB = 128; // 50% duty cycle 
+ 
+void setup() {
+    // Set motor control pins to outputs
+    pinMode(enableA, OUTPUT);
+    pinMode(enableB, OUTPUT);
+    pinMode(motorA1, OUTPUT);
+    pinMode(motorA2, OUTPUT);
+    pinMode(motorB1, OUTPUT);
+    pinMode(motorB2, OUTPUT);
+    
+    // Set motors off for initial state
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, LOW);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, LOW);
+    
+    // Set the motor speed with enable PWM 
+    analogWrite(enableA, motorSpeedA); 
+    analogWrite(enableB, motorSpeedB); 
+}
+ 
+void loop() {
+    // Go forwards
+    go_forward();
+    delay(3000);
+    
+    // Go backwards
+    go_backwards();
+    delay(3000);
+    
+    // Go right
+    go_right();
+    delay(3000);
+    
+    // Go left
+    go_left();
+    delay(3000);
+    
+    // Stop
+    stop_all();
+    delay(3000);
+}
+ 
+/*   
+ *  Forwards, backwards, right, left, stop.
+ */
+void go_forward() {
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+}
+
+void go_backwards() {
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, HIGH);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+}
+
+void go_right() {
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+}
+
+void go_left() {
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, HIGH);
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+}
+
+void stop_all() {
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, LOW);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, LOW);
+}
---
+++ Phase 1A - State Machine/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  // Wait until serial is initialized
+  while (!Serial) ; 
+
+  Serial.print("Initialized"); 
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
