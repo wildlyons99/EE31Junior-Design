62cfad20d3ef0e86b660669953f6c0f4b595cbb8
rren <remy.ren@gmail.com>
2024-04-02 13:16:22 -0400

    additional battery code

--- phase03a-part3/src/batteryadc.cpp
+++ phase03a-part3/src/batteryadc.cpp
@@ -1,21 +1,37 @@
 /* Authors: Chris Bann and Remy Ren
- * 
+ * Library to read the battery percentage value.
  * 
  */
 
+#define RTN_SUCCESS 0
+#define RTN_FAILURE -1
+
 #include <Arduino.h>
 
-
-int batteryPinSetup(int &pinArray, int numberOfPins) {
+/// @brief Set all pins to be observed by ADC
+/// @param int &pinArray 
+/// @param int numberOfPins 
+/// @return int8_t for error checking
+int8_t batteryPinSetup(int &pinArray, int numberOfPins) {
     for (i = 0; i < numberOfPins; i++) {
         pinMode(pinArray[i], INPUT_PULLUP);
     }
+    return RTN_SUCCESS;
 }
 
-int readADC(void) {
-    readAnalog
+/// @brief Reads and gives the ADC value for a pin (A0 to A5)
+/// @param int pinValue
+/// @param int &adcValue 
+/// @return integer ADC value
+int readADC(int pinValue, int &adcValue) {
+    adcValue = analogRead(pinValue);
+    return RTN_SUCCESS;
 }
 
+/// @brief 
+/// @param int adcValue
+/// @param double refVoltage
+/// @return 
 double calculateVoltage(int adcValue, double refVoltage) {
     double referenceVoltage = refVoltage; // Assuming reference voltage is 9.0V
     double r1 = 2.0e6; // R1 = 2M ohms
@@ -28,4 +44,35 @@ double calculateVoltage(int adcValue, double refVoltage) {
     return calculatedVoltage;
 }
 
-void display 
+// https://forum.arduino.cc/t/battery-level-interpretation/1103521/36 MAP PERCENTAGE!
+double calculateMappedPercent() {
+
+}
+
+/// @brief  Sets which LED pins to display battery percent Low, Med, High
+/// @param  int &pinArray -> [0] Low, [1] Medium, [2] High 
+/// @param  int numberOfPins
+/// @return none
+void displayPercentLED() {
+
+} 
+
+/// @brief  Displays the current battery percentage via 3 LEDs -> Low, Med, High
+/// @param  int &pinArray -> [0] Low, [1] Medium, [2] High 
+/// @param  int numberOfPins
+/// @param  double percent
+/// @return none
+void displayPercentLED(double percent) {
+    if {
+
+    }
+    else if {
+
+    }
+    else if {
+
+    }
+    else {
+
+    }
+} 
