438d062eddf0c7732e2e6e96f521489a27369eaa
rren <remy.ren@gmail.com>
2024-04-09 12:48:09 -0400

    serial for battery

--- phase03a-part3/src/main.cpp
+++ phase03a-part3/src/main.cpp
@@ -12,16 +12,20 @@ int motorPercent = 0;
 int pinLogic = A0;
 int pinMotor = A1;
 int pinsADC[] = {pinLogic, pinMotor};
-int pinsLED[] = {A0, A1, A2}; // Replace pins as needed. Low, Med, High.
+int pinsLED[] = {11, 12, 13}; // Replace pins as needed. Low, Med, High.
 
 void setup() {
-    batteryPinSetup(pinsADC, 2);
-    pinsLEDSetup(pinsLED, 3);
+    // batteryPinSetup(pinsADC, 2);
+    // pinsLEDSetup(pinsLED, 3);
+    Serial.begin(9600);
+    Serial.print("yay!\n");
 }
  
 void loop() {
-    motorPercent = getPercentDisplayLED(6.0, 10.2, pinLogic, pinsLED);
-    logicPercent = getPercent(4.0, 5.0, pinLogic);
-    printf("Motor: %i%, Logic: %i%\n", motorPercent, logicPercent);
-    delay(0.1);
+    // motorPercent = getPercentDisplayLED(6.0, 10.2, pinMotor, pinsLED);
+    // Serial.print("Motor: %i%, Logic: %i%\n", motorPercent, logicPercent);
+    Serial.print("Hello World!\n");
+    // logicPercent = getPercent(4.0, 5.0, pinLogic);
+    // printf("Motor: %i%, Logic: %i%\n", motorPercent, logicPercent);
+    delay(500);
 }   
